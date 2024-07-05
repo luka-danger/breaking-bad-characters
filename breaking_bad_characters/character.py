87a28c857de8fff234b8a03c6871244d4d49280f
luka-danger <lukathevegan@gmail.com>
2024-07-05 16:25:37 -0600

    Create Tuco and Jesse

--- breaking_bad_characters/character.py
+++ breaking_bad_characters/character.py
@@ -8,11 +8,12 @@
     '''
 
 class Character: 
-    def __init__(self, name, phrase, job, action, portrayed_by):
+    def __init__(self, name, phrase, job, action, description, portrayed_by):
         self.name = name
         self.phrase = phrase
         self.job = job
         self.action = action
+        self.description = description
         self.portrayed_by = portrayed_by
 
     '''
@@ -49,6 +50,7 @@ class Character:
             'Phrase': self.phrase,
             'Occupation': self.job,
             'Action': self.action,
+            'Description': self.description,
             'Portrayed By': self.portrayed_by
         }
     
@@ -66,7 +68,8 @@ class Character:
     '''
     def print_info(self):
         print(f'Name: {self.name}, Phrase: {self.phrase}, Occupation: {self.job}, '
-              f'Action: {self.action}, Portrayed By: {self.portrayed_by} ')
+              f'Action: {self.action}, Description: {self.description}, '
+              f' Portrayed By: {self.portrayed_by} ')
         
     
 class Walt(Character):
@@ -76,6 +79,9 @@ class Walt(Character):
             phrase = 'Everything I do, I do for this family.',
             job = 'Teacher',
             action = 'Cook',
+            description = 'An overqualified high school chemistry teacher that \
+            gets sick with cancer. In an effort to provide for his family, he decides to \
+            partner with former student, Jesse Pinkman, to produce and sell methamphetamine.',
             portrayed_by = 'Bryan Cranston'
         )
         self.alias = 'Heisenberg'
@@ -88,6 +94,11 @@ class Heisenberg(Walt):
         self.phrase = 'Say my name.'
         self.job = 'Drug Kingpin'
         self.action = 'Intimidate'
+        self.description = 'Initially starting as an alias for Walter White, Heisenberg is' \
+            'a ruthless methamphetamine kingpin that will do anything to build his empire. He' \
+            'gets what he wants through intimidation, intricate lies, manipulating others, and' \
+            'sometimes killing anyone who stands in his way. He represents the egotistical and sociopathic' \
+            'side of Walter White.'
 
 class Jesse(Character):
     def __init__(self):
@@ -96,6 +107,20 @@ class Jesse(Character):
             phrase = 'Yo, yo, yo!',
             job = 'Meth Cook',
             action = 'Making Fat Stacks',
+            description = 'A drugee burnout and former student of Walter White. Jesse dabbles in' \
+            'drug dealing, but eventually partners with Walter White (Heisenberg) to build a massive' \
+            'drug empire.',
             portrayed_by = 'Aaron Paul'
+        )
+
+class Tuco(Character):
+    def __init__(self):
+        super().__init__(
+            name = 'Tuco',
+            phrase = 'Tight, tight, yeah!',
+            job = 'Drug Distributor',
+            action = 'Snorting drugs off a bowie knife and beating his homies when they diss him',
+            description = 'A sadistic, high-level, drug distributor for the Salamanca cartel',
+            portrayed_by = 'Raymond Cruz'
         )
     
