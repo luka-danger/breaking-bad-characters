4cd8b396bde4d30f0cf2ccaf3e81ae0f6c452ae1
luka-danger <lukathevegan@gmail.com>
2024-07-05 15:15:38 -0600

    Initial Commit
    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='breaking_bad_characters',
+    version='0.1',
+    description='A package where you can use Breaking Bad character info',
+    long_description=open('README.md').read(),
+    url='https://github.com/luka-danger/breaking-bad-characters.git',
+    author='Luka Danger'
+)
